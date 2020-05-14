603a00d595ee9d27a888b81b58340513bbcd829d
LolWastedJS <ml2dofficial@gmail.com>
2020-05-14 04:50:18 -0700

    Export shit.

---
+++ src/lib/index.ts
@@ -0,0 +1,13 @@
+/**
+ * Copyright ©️ Kyflx Developments. All rights reserved.
+ * Licensed under the MIT License. See LICENSE file in the project root for full license information.
+ */
+
+export * from "./Kysion";
+
+export * from "./class/base/Piece";
+export * from "./class/base/Registry";
+
+export * from "./util/Constants";
+export * from "./util/Stopwatch";
+export * from "./util/Util";
